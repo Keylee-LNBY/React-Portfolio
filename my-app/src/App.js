80aec60e7b16ddf4661d707b9c965e490d1212e4
Keylee <lineberry.kelly@gmail.com>
2020-04-22 17:26:33 -0400

    added footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar';
+import Cards from './components/Cards';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="m12">
       <Navbar />
+      <Cards />
+      <Footer />
     </div>
   );
+};
+
 }
 
 export default App;
---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer class="page-footer">
+        <div class="container grey darken-3">
+          <div class="row">
+            <div class="s12">
+              <h5 class="white-text">Contact Me</h5>
+              <p class="white-text">Phone Number: 434-907-0481</p>
+              <p class="white-text">Email: Lineberry.Kelly@gmail.com</p>
+            </div>
+          </div>
+        </div>
+        <div class="footer-copyright">
+          <div class="container grey darken-4 white-text">
+            © 2020 Kelly Lineberry
+          </div>
+        </div>
+      </footer>
+    );
+}
+
+export default Footer;
