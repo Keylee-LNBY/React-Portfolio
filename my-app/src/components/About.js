2323b7cb0621e0fad09c97c8453c857c875c5a68
Keylee <lineberry.kelly@gmail.com>
2020-04-23 15:00:49 -0400

    tweeks to the about component

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -12,7 +12,7 @@ const About = () => {
                 </div>
                 <div class="card-stacked grey accent-1">
                     <div class="card-content">
-                        I am a talented coder with an interest in building the most effective, user-friendly, and beautiful software possible. I come from a background in higher-education, eco-tourism, and corporate finance which has given me the opportunity to collaborate with a large variety of people. I have experience with marketing, management, corporate culture, and human resources. I am currently sharpening my development and coding skills through a Trilogy Bootcamp hosted at the University of Denver, which will wrap up in April 2020.
+                        I am a Full Stack Web Developer with an interest in building the most effective, user-friendly, and beautiful software possible. I come from a background in higher-education, eco-tourism, and corporate finance which has given me the opportunity to collaborate with a large variety of people. I have experience with marketing, management, corporate culture, and human resources. I am currently sharpening my development and coding skills through a Trilogy Bootcamp hosted at the University of Denver, which will wrap up in April 2020.
                         <br/>
                         Outside of work I love volunteering at organic farms, learning new programming languages, working my way through every recipe in the family cookbook, and indulging my love for seeing new places.
                     </div>
